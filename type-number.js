6e3801b98f2edf4783872b330ef46d5965bac475
NiedsonBarros <barrosniedson@gmail.com>
2022-02-23 04:57:22 -0300

    exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,10 @@
+// tipo Number
+
+function ganhoPorHora(salario, horasTrabalhadasNoMes) {
+
+    const salarioHora = (salario / horasTrabalhadasNoMes); 
+  
+    return salarioHora;
+  }
+
+ganhoPorHora(3000, 176);
