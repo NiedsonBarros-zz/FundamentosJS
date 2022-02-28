f80eac6f965b560e080925516a6b7cb14dec40c8
NiedsonBarros <barrosniedson@gmail.com>
2022-02-28 17:09:21 -0300

    Adicionados exemplos de erros e console

--- exemplos.js
+++ exemplos.js
@@ -28,3 +28,4 @@
 // console.log(Number(usuarioConectado));
 // usuarioConectado = true;
 // console.log(Number(usuarioConectado));
+
--- script.js
+++ script.js
@@ -7,15 +7,16 @@
 //console.log(area);
 //var area;
 
-let forma = 'retângulo'
-let altura = 5;
-let comprimento = 7;
-let area;
+//let forma = 'retângulo'
+//let altura = 5;
+//let comprimento = 7;
+//let area;
 
-if(forma === 'retângulo'){
-    area = altura * comprimento;
-} else {
-    area = (altura * comprimento) / 2;
-}
+//if(forma === 'retângulo'){
+//    area = altura * comprimento;
+//} else {
+ //   area = (altura * comprimento) / 2;
+//}
 
-console.log(area)
+//console.log(area)
+
